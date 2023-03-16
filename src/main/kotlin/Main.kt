6c04f55c048efc3ef0cce3434c01c62f7e345c46
Richard Tichý <tichy@artify.zone>
2023-03-16 19:19:39 +0100

    Implement solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,25 @@
 fun main() {
-    println("Hello World!")
+    (1..readln().toInt()).forEach { _ ->
+        val n = readln().toInt()
+        val a = readln().split(' ').map { it.toInt() }
+        val b = readln().split(' ').map { it.toInt() }
+
+        val moved = run {
+            var i = n - 1
+            var j = n - 1
+
+            while (0 <= i && 0 <= j) {
+                if(a[i] != b[j]) {
+                    i--
+                } else {
+                    j--
+                    i--
+                }
+            }
+
+            j + 1
+        }
+        
+        println(moved)
+    }
 }
